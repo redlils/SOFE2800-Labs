44e2ee6a625103237a0820689bd88fc2e040239e
Lily Redpath <lily.redpath@ontariotechu.net>
2024-10-21 14:44:06 -0400

    Cleanup script.js from initial lab3 submission

--- lab3/script.js
+++ lab3/script.js
@@ -1,24 +1,24 @@
 const searchInfo = {
-    quiries: [],
+    queries: [],
     limit: 5,
     addQuery: function(query) {
-      if (this.quiries.length >= this.limit) {
-        var oldestQuery = this.quiries.shift(); 
+      if (this.queries.length >= this.limit) {
+        var oldestQuery = this.queries.shift(); 
         console.log("Oldest search query has been deleted: ", oldestQuery);
       }
-      this.quiries.push(query);
+      this.queries.push(query);
     }
   }
   
   function validateSearchForm() {
     let x = document.getElementById("searchText").value;
-    
+
     if (x == "") {
       alert("The search query has to be filled out!");
       return false;
     } else {
       searchInfo.addQuery(x);
-      console.log("Recent searches: ", searchInfo.quiries);
+      console.log("Recent searches: ", searchInfo.queries);
       return true;
     }
   }
